db62ce32213005093ab27e5cd5789b6d3bfd3985
Qiyu Yan <yanqiyu@fedoraproject.org>
2025-08-05 16:25:24 +0800

    Handle tmpfiles before mounts in container startup

--- files/mount.sh
+++ files/mount.sh
@@ -1,7 +1,7 @@
 if [ -f /run/.containerenv ]; then
+  # systemd-tmpfiles can fail but we don't care
+  sudo systemd-tmpfiles --create --boot --exclude-prefix=/dev >/dev/null 2>&1 ||:
   # container don't come with systemd that mounts
   # fstab, do it on our own
   sudo mount -a >/dev/null 2>&1 || echo "Failed to handle mounts"
-  # systemd-tmpfiles can fail but we don't care
-  sudo systemd-tmpfiles --create --boot --exclude-prefix=/dev >/dev/null 2>&1 ||:
 fi
