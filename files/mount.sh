56aae0bfeaee4a5bf83240b92c0e4663b1cf9302
Qiyu Yan <yanqiyu@fedoraproject.org>
2025-08-05 16:24:47 +0800

    Handle mounts and tmpfiles in containers
    Check for container environment before mounting and creating tmpfiles.
    Skip /dev prefix when creating tmpfiles as it may fail in containers.

--- files/mount.sh
+++ files/mount.sh
@@ -1,3 +1,7 @@
-if ! mount | grep -q '/cvmfs/'; then
-    sudo mount -a >/dev/null 2>&1 || echo "Failed to handle mounts"
+if [ -f /run/.containerenv ]; then
+  # container don't come with systemd that mounts
+  # fstab, do it on our own
+  sudo mount -a >/dev/null 2>&1 || echo "Failed to handle mounts"
+  # systemd-tmpfiles can fail but we don't care
+  sudo systemd-tmpfiles --create --boot --exclude-prefix=/dev >/dev/null 2>&1 ||:
 fi
